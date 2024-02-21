7706cbac06cade5fd4811d4c64b8a83a2225259a
mukisa256 <ndabiemmanuelmukisa@gmail.com>
2024-02-21 19:02:55 +0300

    :De|

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 
 
 function App() {
-  const[inputValue, setInputValue] =useState('');
-  const handleClick = (value) =>{
+  const [inputValue, setInputValue] = useState('');
+  const handleClick = (value) => {
     setInputValue((prevState) => {
-      if(value =='.' && prevState.includes('.')){
+      if (value == '.' && prevState.includes('.')) {
         return prevState;
-      }else if(value === '0' && prevState === '0'){
+      } else if (value === '0' && prevState === '0') {
         return '0';
       }
       if (prevState.length >= 15) {
@@ -21,58 +21,58 @@ function App() {
   const calculateResult = () => {
     try {
       const result = eval(inputValue);
-      setInputValue(result.toString()); 
+      setInputValue(result.toString());
     } catch (error) {
-      setInputValue('Error'); 
+      setInputValue('Error');
     }
   };
   //clear function 
-  const clearInput = () =>{
+  const clearInput = () => {
     setInputValue('');
   }
   // delete function 
-  const deleteInput = () =>{
-    setInputValue((prevState) => prevState.slice(0,-1));
+  const deleteInput = () => {
+    setInputValue((prevState) => prevState.slice(0, -1));
   };
   return (
 
     <div className="calculator">
       <div className='box-container'>
-      <form >
-        <div className="container">
-          <h1>Calculator</h1>
-        <input type='text' value={inputValue} readOnly placeholder='0'/>
-        </div>
-        <div className="btn">
-          <input type="button" value="De" onClick={deleteInput}></input>
-          <input type="button" value="Clear" onClick={clearInput}></input>
-          <input type="button" value="." onClick={() => handleClick('.')}></input>
-          <input type="button" value="/" onClick={() => handleClick('/')}></input>
-        </div>
-        <div className="btn">
-          <input type="button" value="7" onClick={() =>handleClick('7')}></input>
-          <input type="button" value="8" onClick={() => handleClick('8')}></input>
-          <input type="button" value="9" onClick={() => handleClick('9')}></input>
-          <input type="button" value="+" onClick={() => handleClick('+')}></input>
-        </div>
-        <div className="btn">
-          <input type="button" value="4" onClick={() => handleClick('4')}></input>
-          <input type="button" value="5" onClick={() => handleClick('5')}></input>
-          <input type="button" value="6" onClick={() => handleClick('6')}></input>
-          <input type="button" value="*" onClick={() => handleClick('*')}></input>
-        </div>
-        <div className="btn">
-          <input type="button" value="1" onClick={() => handleClick('1')}></input>
-          <input type="button" value="2" onClick={() => handleClick('2')}></input>
-          <input type="button" value="3" onClick={() => handleClick('3')}></input>
-          <input type="button" value="-" onClick={() => handleClick('-')}></input>
-        </div>
-        <div className="btn">
-          <input type="button" value="00"onClick={() => handleClick('00')} ></input>
-          <input type="button" value="0" onClick={() => handleClick('0')}></input>
-          <input type="button" value="=" className='Eq' onClick={calculateResult}></input>
-        </div>
-      </form>
+        <form >
+          <div className="container">
+            <h1>Calculator</h1>
+            <input type='text' value={inputValue} readOnly placeholder='0' />
+          </div>
+          <div className="btn">
+            <input type="button" value="De" onClick={deleteInput}></input>
+            <input type="button" value="Clear" onClick={clearInput}></input>
+            <input type="button" value="." onClick={() => handleClick('.')}></input>
+            <input type="button" value="/" onClick={() => handleClick('/')}></input>
+          </div>
+          <div className="btn">
+            <input type="button" value="7" onClick={() => handleClick('7')}></input>
+            <input type="button" value="8" onClick={() => handleClick('8')}></input>
+            <input type="button" value="9" onClick={() => handleClick('9')}></input>
+            <input type="button" value="+" onClick={() => handleClick('+')}></input>
+          </div>
+          <div className="btn">
+            <input type="button" value="4" onClick={() => handleClick('4')}></input>
+            <input type="button" value="5" onClick={() => handleClick('5')}></input>
+            <input type="button" value="6" onClick={() => handleClick('6')}></input>
+            <input type="button" value="*" onClick={() => handleClick('*')}></input>
+          </div>
+          <div className="btn">
+            <input type="button" value="1" onClick={() => handleClick('1')}></input>
+            <input type="button" value="2" onClick={() => handleClick('2')}></input>
+            <input type="button" value="3" onClick={() => handleClick('3')}></input>
+            <input type="button" value="-" onClick={() => handleClick('-')}></input>
+          </div>
+          <div className="btn">
+            <input type="button" value="00" onClick={() => handleClick('00')} ></input>
+            <input type="button" value="0" onClick={() => handleClick('0')}></input>
+            <input type="button" value="=" className='Eq' onClick={calculateResult}></input>
+          </div>
+        </form>
       </div>
     </div>
 
